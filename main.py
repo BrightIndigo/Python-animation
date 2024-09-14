d176463df3cc810d01a457c85f1046ecf7965f45
BrightIndigo <128691376+BrightIndigo@users.noreply.github.com>
2024-09-15 01:17:10 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,34 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
+from matplotlib.animation import FuncAnimation
 
+#fig and axes
 fig, ax = plt.subplots()
-t = np.linspace(0, 3, 40)
-g = -9.81
-v0 = 12
-z = g * t**2 / 2 + v0 * t
+xdata, ydata = [], []
+ln, = plt.plot([], [], 'b', animated=True)
 
-v02 = 5
-z2 = g * t**2 / 2 + v02 * t
-
-scat = ax.scatter(t[0], z[0], c="b", s=5, label=f'v0 = {v0} m/s')
-line2 = ax.plot(t[0], z2[0], label=f'v0 = {v02} m/s')[0]
-ax.set(xlim=[0, 3], ylim=[-4, 10], xlabel='Time [s]', ylabel='Z [m]')
-ax.legend()
-
+def init():
+    ax.set_xlim(0, 2 * np.pi) #set x-axis limit
+    ax.set_ylim(-1, 1) #Set y-axis limit
+    ax.grid(True) #add grid lines
+    ln.set_data([], [])
+    return ln,
 
 def update(frame):
-    # for each frame, update the data stored on each artist.
-    x = t[:frame]
-    y = z[:frame]
-    # update the scatter plot:
-    data = np.stack([x, y]).T
-    scat.set_offsets(data)
-    # update the line plot:
-    line2.set_xdata(t[:frame])
-    line2.set_ydata(z2[:frame])
-    return (scat, line2)
+    xdata.append(frame)
+    ydata.append(np.cos(frame))
+    if len(xdata) > 50:
+        xdata.pop(0)
+        ydata.pop(0)
+    ln.set_data(xdata, ydata)
+    return ln,
 
+ani = FuncAnimation(fig, update, frames=np.linspace(0, 2*np.pi, 128),
+                    init_func=init, blit=True, interval=50)
 
-ani = animation.FuncAnimation(fig=fig, func=update, frames=40, interval=30)
 plt.show()
